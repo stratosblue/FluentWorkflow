2bf8bd7d844e5fd4a3441e0f31d1a5e5068a7aa7
stratos <stratosblue@outlook.com>
2025-04-09 22:06:48 +0800

    chore: 公开InMemoryWorkflowMessageDispatcher

--- src/FluentWorkflow.Core/MessageDispatch/InMemoryWorkflowMessageDispatcher.cs
+++ src/FluentWorkflow.Core/MessageDispatch/InMemoryWorkflowMessageDispatcher.cs
@@ -6,34 +6,53 @@ using Microsoft.Extensions.Logging;
 
 namespace FluentWorkflow.MessageDispatch;
 
-internal class InMemoryWorkflowMessageDispatcher : IWorkflowMessageDispatcher
+/// <summary>
+/// 基于内存的 <inheritdoc cref="IWorkflowMessageDispatcher"/>
+/// </summary>
+public class InMemoryWorkflowMessageDispatcher : IWorkflowMessageDispatcher
 {
-    #region Private 字段
+    #region Protected 字段
 
-    private readonly ImmutableDictionary<string, WorkflowEventInvokerDescriptor[]> _eventSubscribeDescriptors;
+    /// <summary>
+    /// 工作流程事件执行程序描述符订阅列表
+    /// </summary>
+    protected ImmutableDictionary<string, ImmutableArray<WorkflowEventInvokerDescriptor>> EventSubscribeDescriptors { get; }
 
-    private readonly ILogger _logger;
+    /// <inheritdoc cref="ILogger"/>
+    protected ILogger Logger { get; }
 
-    private readonly IObjectSerializer _objectSerializer;
+    /// <inheritdoc cref="IObjectSerializer"/>
+    protected IObjectSerializer ObjectSerializer { get; }
 
-    private readonly IServiceScopeFactory _serviceScopeFactory;
+    /// <inheritdoc cref="IServiceScopeFactory"/>
+    protected IServiceScopeFactory ServiceScopeFactory { get; }
 
-    #endregion Private 字段
+    #endregion Protected 字段
 
     #region Public 构造函数
 
+    /// <summary>
+    /// <inheritdoc cref="InMemoryWorkflowMessageDispatcher"/>
+    /// </summary>
+    /// <param name="serviceScopeFactory"></param>
+    /// <param name="workflowBuildStates"></param>
+    /// <param name="objectSerializer"></param>
+    /// <param name="logger"></param>
     public InMemoryWorkflowMessageDispatcher(IServiceScopeFactory serviceScopeFactory,
                                              WorkflowBuildStateCollection workflowBuildStates,
                                              IObjectSerializer objectSerializer,
                                              ILogger<InMemoryWorkflowMessageDispatcher> logger)
     {
         ArgumentNullException.ThrowIfNull(workflowBuildStates);
+        ArgumentNullException.ThrowIfNull(serviceScopeFactory);
+        ArgumentNullException.ThrowIfNull(objectSerializer);
+        ArgumentNullException.ThrowIfNull(logger);
 
-        _serviceScopeFactory = serviceScopeFactory ?? throw new ArgumentNullException(nameof(serviceScopeFactory));
-        _objectSerializer = objectSerializer ?? throw new ArgumentNullException(nameof(objectSerializer));
-        _eventSubscribeDescriptors = workflowBuildStates.SelectMany(m => m)
-                                                        .ToImmutableDictionary(m => m.EventName, m => m.ToArray());
-        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        ServiceScopeFactory = serviceScopeFactory;
+        ObjectSerializer = objectSerializer;
+        EventSubscribeDescriptors = workflowBuildStates.SelectMany(m => m)
+                                                       .ToImmutableDictionary(m => m.EventName, m => m.ToImmutableArray());
+        Logger = logger;
     }
 
     #endregion Public 构造函数
@@ -41,23 +60,29 @@ internal class InMemoryWorkflowMessageDispatcher : IWorkflowMessageDispatcher
     #region Public 方法
 
     /// <inheritdoc/>
-    Task IWorkflowMessageDispatcher.PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken)
+    public virtual async Task PublishAsync<TMessage>(TMessage message, CancellationToken cancellationToken)
+        where TMessage : class, IWorkflowMessage, IWorkflowContextCarrier<IWorkflowContext>, IEventNameDeclaration
     {
         ArgumentNullException.ThrowIfNull(message);
 
-        if (!_eventSubscribeDescriptors.TryGetValue(TMessage.EventName, out var invokerDescriptors))
+        if (!EventSubscribeDescriptors.TryGetValue(TMessage.EventName, out var invokerDescriptors))
         {
+            if (await OnNotSubscribedMessageAsync(message, cancellationToken))
+            {
+                return;
+            }
             throw new InvalidOperationException($"Not found event subscriber for {{{message.Id}}}\"{TMessage.EventName}\".");
         }
 
-        var messageJson = _objectSerializer.Serialize(message);
-        var messageClone = _objectSerializer.Deserialize<TMessage>(messageJson)!;
+        var messageJson = ObjectSerializer.Serialize(message);
+        var messageClone = ObjectSerializer.Deserialize<TMessage>(messageJson)!;
 
         _ = Task.Run(async () =>
         {
+            await Task.Yield();
             try
             {
-                await using var serviceScope = _serviceScopeFactory.CreateAsyncScope();
+                await using var serviceScope = ServiceScopeFactory.CreateAsyncScope();
                 var serviceProvider = serviceScope.ServiceProvider;
                 if (invokerDescriptors.Length == 1)
                 {
@@ -77,12 +102,27 @@ internal class InMemoryWorkflowMessageDispatcher : IWorkflowMessageDispatcher
             }
             catch (Exception ex)
             {
-                _logger.LogError(ex, "Error at handle message - {{{Id}}}\"{EventName}\"", message.Id, TMessage.EventName);
+                Logger.LogError(ex, "Error at handle message - {{{Id}}}\"{EventName}\"", message.Id, TMessage.EventName);
             }
-        }, CancellationToken.None);
-
-        return Task.CompletedTask;
+        }, cancellationToken);
     }
 
     #endregion Public 方法
+
+    #region Protected 方法
+
+    /// <summary>
+    /// 在出现未订阅的消息时执行的方法
+    /// </summary>
+    /// <typeparam name="TMessage"></typeparam>
+    /// <param name="message"></param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>返回 <see langword="true"/> 则不抛出异常</returns>
+    protected virtual Task<bool> OnNotSubscribedMessageAsync<TMessage>(TMessage message, CancellationToken cancellationToken)
+        where TMessage : class, IWorkflowMessage, IWorkflowContextCarrier<IWorkflowContext>, IEventNameDeclaration
+    {
+        return Task.FromResult(false);
+    }
+
+    #endregion Protected 方法
 }
